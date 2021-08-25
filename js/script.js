3d0625a886521869c960833d68cbf5d68da99d2f
Darya <darya_01@mail.ru>
2021-08-26 00:45:22 +0300

    Смена изображений в блоке 'Наша команда' при наведении

--- js/script.js
+++ js/script.js
@@ -254,6 +254,30 @@ window.addEventListener('DOMContentLoaded', () => {
   };
   slider();
 
+  // Валидация полей калькулятора
+
+  // Смена фото в блоке "Наша команда" при наведении
+  const toggleImage = () => {
+    const imageBlock = document.querySelector('.command .row');
+
+    const toggleSrc = item => {
+      const oldSrc = item.getAttribute('src');
+      const newSrc = item.dataset.img;
+
+      item.setAttribute('src', newSrc);
+      item.dataset.img = oldSrc;
+    };
+
+    imageBlock.addEventListener('mouseover', event => {
+      const target = event.target;
+      toggleSrc(target);
+    });
+    imageBlock.addEventListener('mouseout', event => {
+      const target = event.target;
+      toggleSrc(target);
+    });
+  };
+  toggleImage();
 
 
 });
